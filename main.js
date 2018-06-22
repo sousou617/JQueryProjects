a8077179725472671bd34644aaf94cae34349f52
Cheryl Brown <38012775+sousou617@users.noreply.github.com>
2018-06-22 11:04:25 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+$(init);
+
+function init() {
+	$(".tool").draggable({
+		helper: 'clone'
+	});
+
+	$(".canvas").droppable({
+		drop: (event, ui) => {
+
+			var node = {
+				position: ui.position;
+				html: ui.draggable[0].innerHTML
+			};
+			// console.log(node);
+		}
+		render(node);
+	})
+}
+
+function render(node) {
+	// console.log(node);
+	var html = "<h3>" + node.html + "<h3>";
+	var dom = $(html).css({
+		position: 'absolute',
+		top: node.position.top,
+		left: node.position.left
+	})
+	$(".canvas").append(dom)
+}
