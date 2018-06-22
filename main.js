9604fc5ee6a7d9d75c3f19d9838388020e76ac9c
Cheryl Brown <38012775+sousou617@users.noreply.github.com>
2018-06-22 11:22:24 -0400

    jquery fix

---
+++ main.js
@@ -0,0 +1,34 @@
+$(init);
+
+function init() {
+	$(".tool").draggable({
+		helper: 'clone'
+	});
+
+	$(".canvas").droppable({
+		drop: (event, ui) => {
+
+			var node = {
+				position: ui.position,
+				html: ui.draggable[0].innerHTML
+		};
+			// console.log(node);
+		
+			if(ui.helper.hasClass('tool')){
+			render(node);
+		}
+	  }
+	})
+}
+
+
+function render(node) {
+	// console.log(node);
+	var html = "<h3>" + node.html + "<h3>";
+	var dom = $(html).css({
+		position: 'absolute',
+		top: node.position.top,
+		left: node.position.left -= $(".canvas").position().left
+	})
+	$(".canvas").append(dom.draggable());
+}
