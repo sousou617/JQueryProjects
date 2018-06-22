04739761b8a19977df012a8aa069412334293746
Cheryl Brown <38012775+sousou617@users.noreply.github.com>
2018-06-22 11:40:15 -0400

    remove duplicate

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 $(init);
 
 function init() {
@@ -33,35 +33,3 @@ function render(node) {
 	})
 	$(".canvas").append(dom.draggable());
 }
-=======
-$(init);
-
-function init() {
-	$(".tool").draggable({
-		helper: 'clone'
-	});
-
-	$(".canvas").droppable({
-		drop: (event, ui) => {
-
-			var node = {
-				position: ui.position;
-				html: ui.draggable[0].innerHTML
-			};
-			// console.log(node);
-		}
-		render(node);
-	})
-}
-
-function render(node) {
-	// console.log(node);
-	var html = "<h3>" + node.html + "<h3>";
-	var dom = $(html).css({
-		position: 'absolute',
-		top: node.position.top,
-		left: node.position.left
-	})
-	$(".canvas").append(dom)
-}
->>>>>>> a8077179725472671bd34644aaf94cae34349f52
